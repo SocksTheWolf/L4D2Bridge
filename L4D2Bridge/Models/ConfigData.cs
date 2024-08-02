7b1ddbc800e7e86ce2dbebe871a3f1014dfc4b97
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-02 15:46:20 -0700

    Make the settings system more dynamic
    Instead of hardcoded

--- L4D2Bridge/Models/ConfigData.cs
+++ L4D2Bridge/Models/ConfigData.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Newtonsoft.Json;
 
 namespace L4D2Bridge.Models
 {
+    /*** Base Types/Classes ***/
     using RequiredFieldContainer = List<string>;
+    public abstract class SettingsVerifier
+    {
+        public abstract void AddRequiredFields(ref RequiredFieldContainer RequiredFieldObj);
+    }
 
+    /*** Settings for Tiltify ***/
     [JsonObject(MemberSerialization.OptOut)]
-    public class TiltifySettings
+    public class TiltifySettings : SettingsVerifier
     {
         [JsonProperty(Required = Required.Always)]
         public bool Enabled { get; set; } = true;
@@ -33,15 +40,16 @@ namespace L4D2Bridge.Models
         [JsonProperty]
         public int PollingInterval { get; set; } = 5;
 
-        public void AddRequiredFields(ref RequiredFieldContainer RequiredFieldObj)
+        public override void AddRequiredFields(ref RequiredFieldContainer RequiredFieldObj)
         {
             if (Enabled)
                 RequiredFieldObj.AddRange([ClientID, ClientSecret, CampaignID]);
         }
     }
 
+    /*** Settings for Twitch ***/
     [JsonObject(MemberSerialization.OptOut, ItemRequired = Required.Always)]
-    public class TwitchSettings
+    public class TwitchSettings : SettingsVerifier
     {
         [JsonProperty]
         public bool Enabled { get; set; } = false;
@@ -55,7 +63,7 @@ namespace L4D2Bridge.Models
         [JsonProperty]
         public string OAuthToken { get; set; } = string.Empty;
 
-        public void AddRequiredFields(ref RequiredFieldContainer RequiredFieldObj)
+        public override void AddRequiredFields(ref RequiredFieldContainer RequiredFieldObj)
         {
             if (Enabled)
                 RequiredFieldObj.AddRange([ChannelName, BotUserName, OAuthToken]);
@@ -81,11 +89,11 @@ namespace L4D2Bridge.Models
         [JsonProperty]
         public string RConPassword { get; set; } = string.Empty;
 
-        /*** Twitch Connection Information ***/
+        /*** Twitch Settings ***/
         [JsonProperty(PropertyName = "twitch", NullValueHandling = NullValueHandling.Include, Required=Required.AllowNull)]
         public TwitchSettings? TwitchSettings { get; set; }
 
-        /*** Tiltify API Settings ***/
+        /*** Tiltify Settings ***/
         [JsonProperty(PropertyName = "tiltify", NullValueHandling = NullValueHandling.Include, Required = Required.AllowNull)]
         public TiltifySettings? TiltifySettings { get; set; }
 
@@ -113,13 +121,18 @@ namespace L4D2Bridge.Models
                         configData.IsValid = true;
                         RequiredFieldContainer checkIfNotNull = [configData.RConServerIP, configData.RConPassword];
 
-                        // Add important Tiltify configs if we're using it.
-                        if (configData.TiltifySettings != null)
-                            configData.TiltifySettings.AddRequiredFields(ref checkIfNotNull);
-
-                        // Add important Twitch configs if we're using it.
-                        if (configData.TwitchSettings != null)
-                            configData.TwitchSettings.AddRequiredFields(ref checkIfNotNull);
+                        // Get all of our properties in the config class of type SettingsVerifier
+                        var verifyProperties = configData.GetType().GetProperties().Where(prop => prop.PropertyType.IsSubclassOf(typeof(SettingsVerifier)));
+                        foreach (PropertyInfo? property in verifyProperties)
+                        {
+                            // Attempt to get the value of the property if it is set
+                            object? Value = property?.GetValue(configData);
+                            if (Value != null)
+                            {
+                                // We have the object, so cast it to the SettingsVerifier class and add the required fields
+                                ((SettingsVerifier)Value).AddRequiredFields(ref checkIfNotNull);
+                            }
+                        }
 
                         // Check if any of the settings are invalid.
                         if (checkIfNotNull.Any(it => string.IsNullOrEmpty(it)))
