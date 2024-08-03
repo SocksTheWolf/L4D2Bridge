6fd8afd9fd280788f25e3d0f8d912fc3457c15c7
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-02 23:45:26 -0700

    Make a publish series of files

---
+++ publish.bat
@@ -0,0 +1,12 @@
+@echo off
+rm -f Release.zip
+rem Move to publish folder
+cd "L4D2Bridge.Desktop\publish\"
+rem do some cleanup
+rm -f *.pdb
+rm -f L4D2Bridge.exe
+rem rename binary
+rename L4D2Bridge.Desktop.exe L4D2Bridge.exe
+rem make zip
+7z a ..\..\Release.zip *.*
+@echo on
