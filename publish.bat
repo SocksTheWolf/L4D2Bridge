7a589a6aac7a392e11ac2bb72bafe01bd1cd6c32
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-16 14:26:15 -0700

    Update publish script

--- publish.bat
+++ publish.bat
@@ -4,9 +4,12 @@ rem Move to publish folder
 cd "L4D2Bridge.Desktop\publish\"
 rem do some cleanup
 rm -f *.pdb
+if exist L4D2Bridge.Desktop.exe (
 rm -f L4D2Bridge.exe
 rem rename binary
 rename L4D2Bridge.Desktop.exe L4D2Bridge.exe
+)
+
 rem make zip
-7z a ..\..\Release.zip *.* ..\..\README.md ..\..\LICENSE
+7z a ..\..\Release.zip *.dll *.exe ..\..\README.md ..\..\LICENSE
 @echo on
